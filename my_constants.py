1372529567f30bd7e96e5c27f52ef7229338cd24
rwells <rwells02@qub.ac.uk>
2018-02-20 14:10:27 +0000

    Initial commit

---
+++ my_constants.py
@@ -0,0 +1,7 @@
+ME = 5.972e24       # Earth mass (kg)
+MJ = 1.898e27       # Jupiter mass (kg)
+MS = 1.989e30       # Sun mass (kg)
+AU = 1.496e11       # au (m)
+RS = 695508e3       # Sun radius (m)
+RE = 6.3710e6       # Earth radius (m)
+RJ = 69.911e6       # Jupiter radius (m)
